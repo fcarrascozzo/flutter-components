e645fe8cbae2d68bcf64b42d62501fc791bceaf6
Fernando Dias Carrascozzo <fernandocarrascozzo@gmail.com>
2024-05-28 08:16:34 -0300

    Ajustando e corrigindo inconsistências na explicação do código widgetOpaco.dart

--- widgetOpaco.dart
+++ widgetOpaco.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
-/// Esta classe é uma extensão do AnimatedOpacity, herdando todos os seus atributos.
-/// Ela tem um comportamento específico baseado na visibilidade do widget.
+/// A classe WidgetOpaco estende AnimatedOpacity, herdando todos os seus atributos.
+/// Ela permite alterar a opacidade de um widget filho com uma animação, 
+/// baseada na visibilidade fornecida.
+///
+/// Parâmetros:
+/// - [child]: O widget filho que será animado.
+/// - [isVisible]: Um booleano que determina se o widget é visível (true) ou invisível (false).
+/// - [milliseconds]: A duração da animação em milissegundos (opcional, padrão é 500 ms).
 ///
 /// Comportamento:
-/// - Quando visível: O widget filho (child) é exibido.
-/// - Quando não visível: Um SizedBox vazio é exibido. Isso garante que nenhum espaço seja ocupado, permitindo que o espaço seja utilizado por outro widget.
+/// - Quando [isVisible] é true: O widget filho [child] é exibido com opacidade 1.
+/// - Quando [isVisible] é false: Um [SizedBox] é exibido com opacidade 0, 
+///   permitindo que o espaço seja potencialmente reutilizado por outro widget, 
+///   mas tecnicamente o espaço continua reservado.
 ///
 /// Nota Importante:
-/// O widget filho (child) não é substituído por outro widget. Isso ocorre porque, para que a animação ocorra,
-/// o widget precisa permanecer no local, porém com opacidade 0. Portanto, se um widget secundário fosse usado, ele não seria exibido,
-/// tornando desnecessário o uso do AnimatedOpacity.
+/// Para que a animação ocorra corretamente, o widget animado (seja o [child] ou o [SizedBox])
+/// precisa permanecer no layout, mesmo com opacidade 0.
 
 class WidgetOpaco extends AnimatedOpacity {
   WidgetOpaco({
@@ -19,8 +26,11 @@ class WidgetOpaco extends AnimatedOpacity {
     required bool isVisible,
     int? milliseconds,
   }) : super(
+          // Define a opacidade com base na visibilidade
           opacity: isVisible ? 1 : 0,
+          // Define a duração da animação
           duration: Duration(milliseconds: milliseconds ?? 500),
+          // Se o widget é visível, exibe o child, caso contrário exibe um SizedBox
           child: isVisible ? child : SizedBox(),
         );
 }
